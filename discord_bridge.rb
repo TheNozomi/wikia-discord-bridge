fdaa79891f651ca5733486d8ec492271e2b3e69a
Nozomi <ElNobDeTfm@users.noreply.github.com>
2017-06-19 16:07:44 -0300

    Adding the plugin file

---
+++ discord_bridge.rb
@@ -0,0 +1,59 @@
+require_relative '../plugin'
+require_relative '../client'
+require 'httparty'
+require 'discordrb'
+
+$discordbot = Discordrb::Bot.new token: "your token here", client_id: #your client id here, without quotes
+$channel_id = #your channel id here, without quotes
+
+class Chatbot::DiscordBridge
+  include Chatbot::Plugin
+
+  def discord_send(content)
+    $discordbot.send_message($channel_id, content)
+  end
+
+  match /(.*)/, :use_prefix => false, :method => :on_message
+  listen_to :join, :on_join
+  listen_to :part, :on_part
+  listen_to :kick, :on_kick
+  listen_to :ban, :on_ban
+
+  def on_message(user, message)
+    if message.start_with? '[b]<'
+      return
+    elsif message.start_with? '/me '
+      discord_send("**<#{user.name}>** * #{user.name} #{message.sub(/\/me /, '')}")
+    else
+      discord_send("**<#{user.name}>** #{message.gsub(/\[b]|\[\/b]/, '**').gsub(/\[i]|\[\/i]/, '*').gsub(/\[s]|\[\/s]/, '~~').gsub(/\[u]|\[\/u]/, '__').gsub(/\[code]|\[\/code]/, '`').gsub(/\[(?:img|audio|video)="/, 'http://').gsub(/\[(?:yt|youtube)="/, 'https://youtu.be/')}")
+    end
+  end
+
+  def on_join(data)
+    @client.send_msg data
+    discord_send("~ #{data['attrs']['name']} has joined the chat. ~")
+  end
+
+  def on_part(data)
+    @client.send_msg data
+    discord_send("~ #{data['attrs']['name']} has left the chat. ~")
+  end
+
+  def on_kick(data)
+    discord_send("~ #{data['attrs']['kickedUserName']} has been kicked by #{data['attrs']['moderatorName']}. ~")
+  end
+
+  def on_ban(data)
+    discord_send("~ #{data['attrs']['kickedUserName']} has been banned by #{data['attrs']['moderatorName']}. ~")
+  end
+
+  def initialize(bot)
+    super(bot)
+    $discordbot.message(in: $channel_id) do |event|
+      username = event.author.display_name
+      message = event.content
+      bot.send_msg "[b]<#{username}>[/b] #{message.gsub(/\*\*(.*)\*\*/, '[b]\1[/b]').gsub(/\*(.*)\*/, '[i]\1[/i]').gsub(/~~(.*)~~/, '[s]\1[/s]').gsub(/__(.*)__/, '[u]\1[/u]').gsub(/`(.*)`/, '[code]\1[/code]')}"
+    end
+  end
+
+end
